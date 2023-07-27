ae7ae8d9ead5ca4fd0a7c661017cf832cab64efa
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-07-27 14:15:35 +0530

    styles and reset button added to MyComponent

--- react-tricky-concepts-practice-and-revise/src/components/MyComponent.js
+++ react-tricky-concepts-practice-and-revise/src/components/MyComponent.js
@@ -6,17 +6,71 @@ function MyComponent() {
   const handleClick = () => {
     console.log("before setTimeout!!");
     setTimeout(() => {
-      setCount(count + 1);
-      // setCount((prevValue) => prevValue + 1);
+      setCount((prevValue) => prevValue + 1);
+      // setCount(count + 1);
       console.log("State updated:", count);
     }, 1000);
     console.log("after setTimeout");
   };
 
+  const resetHandler = () => setCount(0);
+
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={handleClick}>Increment</button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        background: "#f0f0f0",
+      }}
+    >
+      <div
+        style={{
+          padding: "20px",
+          border: "2px solid #333",
+          borderRadius: "10px",
+          background: "#fff",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          minWidth: "300px",
+          textAlign: "center",
+        }}
+      >
+        <h2 style={{ color: "#333" }}>Updated the State Using Function Form</h2>
+        <p style={{ fontSize: "36px", fontWeight: "bold", margin: "20px 0" }}>
+          Count: {count}
+        </p>
+        <div>
+          <button
+            onClick={handleClick}
+            style={{
+              background: "#007bff",
+              color: "#fff",
+              padding: "10px 20px",
+              fontSize: "16px",
+              borderRadius: "5px",
+              cursor: "pointer",
+              marginRight: "10px",
+            }}
+          >
+            Increment
+          </button>
+          <button
+            onClick={resetHandler}
+            style={{
+              background: "#dc3545",
+              color: "#fff",
+              padding: "10px 20px",
+              fontSize: "16px",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
