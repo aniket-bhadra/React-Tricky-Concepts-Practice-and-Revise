8f30bb83d2f58d8e9e55e0e5b8ef272a8f5b8ec9
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-11-21 19:08:38 +0530

    basic structure crated & added

--- react-tricky-concepts-practice-and-revise/src/App.js
+++ react-tricky-concepts-practice-and-revise/src/App.js
@@ -4,15 +4,17 @@ import MyComponent2 from './components/MyComponent2';
 import MyComponent3 from './components/MyComponent3';
 import MyComponent4 from './components/MyComponent4';
 import MyComponent5 from './components/MyComponent5';
+import TestingRerendering from './components/TestingRerendering';
 
 function App() {
   return (
     <div>
-     <MyComponent />
+     {/* <MyComponent /> */}
      {/* <MyComponent4 /> */}
      {/* <MyComponent5 /> */}
      {/* <MyComponent2 /> */}
      {/* <MyComponent3 /> */}
+     <TestingRerendering />
     </div>
   );
 }
---
+++ react-tricky-concepts-practice-and-revise/src/components/TestingRerendering.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestingRerendering = () => {
+  return (
+    <div>TestingRerendering</div>
+  )
+}
+
+export default TestingRerendering
