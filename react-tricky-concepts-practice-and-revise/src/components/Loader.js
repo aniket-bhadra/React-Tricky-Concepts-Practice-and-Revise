b03d2f2a2b0df76abd6fe31f2e157d5b6b88d6a9
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-11-22 11:18:55 +0530

    case-1 of re rending of child when parent rerender is tested in every possible scenario of prop drilling

--- react-tricky-concepts-practice-and-revise/src/components/Loader.js
+++ react-tricky-concepts-practice-and-revise/src/components/Loader.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Loader = () => {
-  return <div>this is Loader......</div>;
+const Loader = ({ loader }) => {
+  console.log("hello from loader");
+  return <div>{loader && <div>this is Loader......</div>} </div>;
 };
 
 export default Loader;
--- react-tricky-concepts-practice-and-revise/src/components/TestingRerendering.js
+++ react-tricky-concepts-practice-and-revise/src/components/TestingRerendering.js
@@ -4,10 +4,16 @@ import Loader from "./Loader";
 
 const TestingRerendering = () => {
   const [show, setShow] = useState(null);
+  const [seen, setSeen] = useState(null);
+  console.log("TestingRerendering compoenent");
+  console.log("show state varible value is ", show);
   return (
     <div>
-      TestingRerendering
-      <Loader />
+      <button onClick={() => setShow((prevState) => !prevState)}>
+        TestingRerendering
+      </button>
+      <Loader loader={show} />
+      <button onClick={() => setSeen((prevState) => !prevState)}>seen</button>
     </div>
   );
 };
